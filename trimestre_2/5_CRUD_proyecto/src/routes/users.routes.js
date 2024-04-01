5c4e91d5ca379d3dd978a1c74ee21f662b534aa5
Armando Velasquez <javt28@outlook.com>
2024-04-01 17:22:12 -0500

    Conexión exitosa a la base de datos PostgreSQL

--- trimestre_2/5_CRUD_proyecto/src/routes/users.routes.js
+++ trimestre_2/5_CRUD_proyecto/src/routes/users.routes.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
+const pool = require('../db')
 
 const router = Router();
 
-router.get('/users', (req, res) => {
-    res.send('Retornando una lista de usuarios');
+router.get('/users', async (req, res) => {
+    const result = await pool.query('SELECT * FROM users;')
+    res.json(result.rows[0].name)
 });
 
 router.get('/users/:id', (req, res) => {
