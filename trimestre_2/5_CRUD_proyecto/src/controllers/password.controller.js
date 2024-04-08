6148ed89ed7619a0f3a4f083510e47ed61f08086
Armando Velasquez <javt28@outlook.com>
2024-04-08 04:14:51 -0500

    CRUD: Controller password actualizado

--- trimestre_2/5_CRUD_proyecto/src/controllers/password.controller.js
+++ trimestre_2/5_CRUD_proyecto/src/controllers/password.controller.js
@@ -4,36 +4,59 @@ const setPassword = async (req, res, next) => {
     const { id } = req.params;
     const { password } = req.body;
 
+    let client;
+
     try {
-        const result = await pool.query(
-            `INSERT INTO "ABB".user_credentials (id_user, password)
+        client = await pool.connect();
+        await client.query('BEGIN');
+
+        const result = await client.query({
+            text: `INSERT INTO "ABB".user_credentials (id_user, password)
             VALUES ($1 ,$2);`,
-            [id, password]
-        );
+            values: [id, password]
+        });
+
+        await client.query('COMMIT');
 
         res.json(result.rows);
     } catch (err) {
+        await client.query('ROLLBACK');
         next(err);
+    } finally {
+        client.release();
     }
 }
+
 
 const updatePassword = async (req, res, next) => {
     const { id } = req.params;
     const { password } = req.body;
 
+    let client;
+
     try {
-        const result = await pool.query(
-            `UPDATE "ABB".user_credentials  
+        client = await pool.connect();
+        await client.query('BEGIN');
+
+        const result = await client.query({
+            text: `UPDATE "ABB".user_credentials  
             SET password = $1 
             WHERE id_user = $2`,
-            [password, id]
-        );
+            values: [password, id]
+        });
+
+        await client.query('COMMIT');
 
         res.json(result.rows);
     } catch (err) {
+        await client.query('ROLLBACK');
         next(err);
+    } finally {
+        client.release();
     }
 }
+
+
 module.exports = {
     setPassword,
     updatePassword
