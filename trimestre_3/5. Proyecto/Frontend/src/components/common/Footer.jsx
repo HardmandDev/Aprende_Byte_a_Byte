0965c3fa9da91b42f9d5f05efe945964213bd93c
BrainyIng <nikovelariza@gmail.com>
2024-06-04 16:46:58 -0500

    Footer: Eliminando position fixed

--- trimestre_3/5. Proyecto/Frontend/src/components/common/Footer.jsx
+++ trimestre_3/5. Proyecto/Frontend/src/components/common/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="footer" style={{textAlign: 'center', background: '#000', color: '#fff', position: 'fixed', bottom: 0, width: '100%' }}>
+    <footer className="footer" style={{textAlign: 'center', background: '#000', color: '#fff', marginTop: '5px', bottom: 0, width: '100%' }}>
       <p>© 2023 Aprende Byte a Byte. All rights reserved.</p>
     </footer>
   );
