026398952e1ae2417ddd76fe403071733e5b2628
Armando Velasquez <javt28@outlook.com>
2024-06-08 03:09:33 -0500

    Frontend: Pequeños ajustes en rutas y otros.

--- trimestre_3/5. Proyecto/Frontend/src/App.jsx
+++ trimestre_3/5. Proyecto/Frontend/src/App.jsx
@@ -3,9 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
 
-import Home from './pages/Home'
-import Profile from './pages/Profile'
 import Index from './components/guest/Index';
+import Profile from './pages/Profile'
 import SignUp from './components/guest/SignUp';
 
 
@@ -15,10 +14,9 @@ function App() {
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Index />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/index" element={<Index />} />
       </Routes>
       <Footer />
     </>
--- trimestre_3/5. Proyecto/Frontend/src/components/guest/SignUp.jsx
+++ trimestre_3/5. Proyecto/Frontend/src/components/guest/SignUp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
