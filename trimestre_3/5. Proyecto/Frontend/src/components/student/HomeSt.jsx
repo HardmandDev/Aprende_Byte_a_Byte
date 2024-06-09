768bfaf0c5ee3c97b3d46f0abe50f325c4137313
BrainyIng <nikovelariza@gmail.com>
2024-06-08 23:18:53 -0500

    Frontend : Agregando container-2 a HomeSt

--- trimestre_3/5. Proyecto/Frontend/src/components/student/HomeSt.jsx
+++ trimestre_3/5. Proyecto/Frontend/src/components/student/HomeSt.jsx
@@ -6,23 +6,46 @@ import { Button } from "@/components/ui/button"
 
 export default function HomeSt() {
   return (
-    <article className="scrollArea-container">
-      <div >
-        <ScrollArea className="h-[800px] w-[300px] rounded-md border p-7">
+    <article>
+      <header className="scrollArea-container">
+        <ScrollArea className="h-[600px] w-[300px] rounded-md border p-7" style={{borderColor:'gray'}}>
           <div className="flex justify-center items-center mb-4">
-          <img src="src/assets/APRENDE-removebg-preview.png" alt="Logo" className="image-logo"/>
+            <img src="src/assets/APRENDE-removebg-preview.png" alt="Logo" className="image-logo"/>
           </div>
           <aside>
-          <Button variant="ghost" className="w-full">
-            Inicio
-          </Button>
+            <Button variant="outline" className="w-full">
+              Inicio
+            </Button>
+            <strong className="text-course">
+              CURSOS
+            </strong>
+            <Button variant="outline" className="w-full">
+              Aprende Javascript
+            </Button>
           </aside>
         </ScrollArea>
+
         <div className="container-home">
-          <Button variant="ghost" className="w-full">
+          <Button variant="outline" className="w-full" style={{borderColor:'gray'}}>
             (Usuario)
           </Button>
         </div>
+
+      </header>
+      <div className="container-welcome">
+        <ScrollArea className="h-[500px] w-[1000px] ">
+          <strong className="text-welcome">
+            Bienvenido, 
+          </strong>
+          <strong className="text-welcome-2">
+            (Usuario)!
+          </strong>
+          <div className="container-course">
+            <strong>Conceptos Básicos</strong>
+            <h2>Progreso del módulo:</h2>
+            <Button>Continuar Curso</Button>         
+          </div>
+        </ScrollArea>
       </div>
     </article>
   )
