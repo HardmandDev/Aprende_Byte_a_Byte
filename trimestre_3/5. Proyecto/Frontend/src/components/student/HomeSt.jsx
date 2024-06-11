446b8252b7d27470becddd979109368375d62b0a
BrainyIng <nikovelariza@gmail.com>
2024-06-10 21:00:42 -0500

    Frontend : Agregando iconos a los botones

--- trimestre_3/5. Proyecto/Frontend/src/components/student/HomeSt.jsx
+++ trimestre_3/5. Proyecto/Frontend/src/components/student/HomeSt.jsx
@@ -14,13 +14,22 @@ export default function HomeSt() {
           </div>
           <aside>
             <Button variant="outline" className="w-full">
+              <img src="https://university.alchemy.com/assets/home_logo.8e92adf7.svg" alt="Inicio" className="image-home"/>
               Inicio
             </Button>
             <strong className="text-course">
               CURSOS
             </strong>
             <Button variant="outline" className="w-full">
+              <img src="https://university.alchemy.com/assets/js_logo_drawer.384f31cc.svg" alt="Js" className="image-home"/>
               Aprende Javascript
+            </Button>
+            <strong className="text-course">
+              LOGROS
+            </strong>
+            <Button variant="outline" className="w-full">
+              <img src="https://university.alchemy.com/cert-icon.svg" alt="Certficaciones" className="image-home"/>
+              Certificaciones
             </Button>
           </aside>
         </ScrollArea>
