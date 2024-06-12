01517fcb9ffe3fd551f252faaaddf23f76142ba5
Armando Velasquez <hardmanddev@gmail.com>
2024-06-12 00:41:38 -0500

    Backend: fix/Corrigiendo que otros roles relacionados puedan acceder a los endpoints privados.

--- trimestre_3/5. Proyecto/Backend/src/routes/users.routes.js
+++ trimestre_3/5. Proyecto/Backend/src/routes/users.routes.js
@@ -1,24 +1,26 @@
 const { Router } = require('express');
 const router = Router();
 
-const { createUser,
-        updateUser,
-        deleteUser,
-        getUsers,
-        getUserById
+const { 
+    createUser,
+    updateUser,
+    deleteUser,
+    getUsers,
+    getUserById
 } = require('../controllers/users.controller');
 const { loginUser } = require('../controllers/auth.controller');
 
 const { authenticateToken } = require('../middlewares/authenticateToken');
 const { checkRole } = require('../middlewares/checkRole');
 
+// Public routes
 router.post('/register', createUser);
 router.post('/login', loginUser);
 
-// Rutas protegidas
-router.put('/users/:id', authenticateToken, checkRole('support'), updateUser);
-router.delete('/users/:id', authenticateToken, checkRole('support'), deleteUser);
-router.get('/users', authenticateToken, checkRole('support'), getUsers);
-router.get('/users/:id', authenticateToken, checkRole('support'), getUserById);
+// Private routes
+router.get('/users', authenticateToken, checkRole('support' || 'admin'), getUsers);
+router.put('/users/:id', authenticateToken, checkRole('support' || 'admin' || 'student'), updateUser);
+router.delete('/users/:id', authenticateToken, checkRole('support' || 'admin' || 'student'), deleteUser);
+router.get('/users/:id', authenticateToken, checkRole('support' || 'admin' || 'student'), getUserById);
 
 module.exports = router;
