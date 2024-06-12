8601cc9724ca7a1ab7cd60ab79c5cfc74ecfe036
Armando Velasquez <hardmanddev@gmail.com>
2024-06-12 00:21:14 -0500

    Backend: feat/Se agregaron otras rutas protegidas.

--- trimestre_3/5. Proyecto/Backend/src/routes/users.routes.js
+++ trimestre_3/5. Proyecto/Backend/src/routes/users.routes.js
@@ -1,6 +1,12 @@
 const { Router } = require('express');
 const router = Router();
-const { createUser, updateUser } = require('../controllers/users.controller');
+
+const { createUser,
+        updateUser,
+        deleteUser,
+        getUsers,
+        getUserById
+} = require('../controllers/users.controller');
 const { loginUser } = require('../controllers/auth.controller');
 
 const { authenticateToken } = require('../middlewares/authenticateToken');
@@ -11,5 +17,8 @@ router.post('/login', loginUser);
 
 // Rutas protegidas
 router.put('/users/:id', authenticateToken, checkRole('support'), updateUser);
+router.delete('/users/:id', authenticateToken, checkRole('support'), deleteUser);
+router.get('/users', authenticateToken, checkRole('support'), getUsers);
+router.get('/users/:id', authenticateToken, checkRole('support'), getUserById);
 
 module.exports = router;
