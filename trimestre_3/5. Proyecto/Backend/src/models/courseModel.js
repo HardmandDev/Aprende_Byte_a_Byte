80d1a7678d11ea3009c6f1e3e5df0ea6ab204da8
Armando Velasquez <hardmanddev@gmail.com>
2024-06-12 02:17:51 -0500

    Backend: feat/Creando el modelo para crear un curso.

--- trimestre_3/5. Proyecto/Backend/src/models/courseModel.js
+++ trimestre_3/5. Proyecto/Backend/src/models/courseModel.js
@@ -1,5 +1,22 @@
 const pool = require('../db');
 
+// Used by the createCourse function of teacher.controller.js
+const createCourse = async (course, user_id) => {
+    const res = await pool.query(
+        `INSERT INTO courses (
+            user_id,
+            course_name, 
+            description, 
+            image_url,
+            level_id
+            ) 
+            VALUES ($1, $2, $3, $4, $5) 
+            RETURNING *`,
+        [user_id, course.course_name, course.description, course.image_url, course.level_id]
+    );
+    return res.rows[0];
+};
+
 // Used by the approveCourse, rejectCourse and pendingCourse functions of admin.controller.js
 const updateCourseStatus = async (courseId, status) => {
     const res = await pool.query(
@@ -10,5 +27,6 @@ const updateCourseStatus = async (courseId, status) => {
 };
 
 module.exports = {
+    createCourse,
     updateCourseStatus,
 };
